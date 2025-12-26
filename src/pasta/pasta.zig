7326f0f40eb0b35df288b3b522b033ee581cc116
SmileYik <miskyle@outlook.com>
2025-12-26 15:44:37 +0800

    feat: implement dupe method for Pasta to allow deep copy instance.

--- src/pasta/pasta.zig
+++ src/pasta/pasta.zig
@@ -1,4 +1,8 @@
+const Allocator = @import("std").mem.Allocator;
+
 pub const Pasta = struct {
+    const Self = @This();
+
     /// id
     id: ?u64 = null,
 
@@ -25,4 +29,22 @@ pub const Pasta = struct {
     create_at: ?u64 = null,
     expiration_at: ?u64 = null,
     profiles: ?[]const u8 = null,
+
+    pub fn dupe(self: Self, gpa: Allocator) !Pasta {
+        var pasta: Pasta = self;
+        pasta.name = try dupe_str(self.name, gpa);
+        pasta.content = try dupe_str(self.content, gpa);
+        pasta.content_type = try dupe_str(self.content_type, gpa);
+        pasta.attachements = try dupe_str(self.attachements, gpa);
+        pasta.password = try dupe_str(self.password, gpa);
+        pasta.profiles = try dupe_str(self.profiles, gpa);
+        return pasta;
+    }
+
+    fn dupe_str(str: ?[]const u8, gpa: Allocator) !?[]u8 {
+        if (str) |s| {
+            return try gpa.dupe(u8, s);
+        }
+        return null;
+    } 
 };
