93aaa754c01fc7c450f5c062104377de95e08838
SmileYik <miskyle@outlook.com>
2025-12-27 20:40:04 +0800

    feat: implement a pagination struct: PageList

--- src/common/root.zig
+++ src/common/root.zig
@@ -4,3 +4,4 @@ pub const Options = mod.Options;
 pub const DaoType = mod.DaoType;
 pub const Result = @import("result.zig");
 pub const SimpleSqlitePool = @import("simple_sqlite_pool.zig");
+pub const PageList = @import("page_list.zig").PageList;
