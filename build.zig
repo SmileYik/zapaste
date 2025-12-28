c5ab155780cf1078fdb9a892bbd369a152df5897
SmileYik <miskyle@outlook.com>
2025-12-28 18:59:47 +0800

    chore(build): set use_llvm=true

--- build.zig
+++ build.zig
@@ -117,6 +117,7 @@ pub fn build(b: *std.Build) void {
                 .{ .name = "zapaste", .module = mod }
             },
         }),
+        .use_llvm = true
     });
 
     // This declares intent for the executable to be installed into the
